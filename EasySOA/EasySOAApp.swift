fc8219d11f99ad3c3ba247ea985bed0f66423444
Alexey Golovenkov <mobidevelop@gmail.com>
2022-03-05 23:04:44 +0400

    Empty project

---
+++ EasySOA/EasySOAApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasySOAApp.swift
+//  EasySOA
+//
+//  Created by Alex Golovenkov on 05.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct EasySOAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
